87427d8be588ac673f4a5fe10e5e5d8a681da71f
kody2248 <kody2248@gmail.com>
2022-11-11 15:09:08 -0500

    Added env support

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['./configs/.env.local', './configs/.env.production'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
