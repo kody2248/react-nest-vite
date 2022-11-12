3c7fa3e90fc2feb35d964e79e7dfb00d3cdd35b1
kody2248 <kody2248@gmail.com>
2022-11-11 22:19:45 -0500

    Added env support to server

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,12 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['./configs/.env.local', './configs/.env.production'],
+      envFilePath: [
+        './configs/.env',
+        './configs/.env.local',
+        './configs/.env.production',
+      ],
     }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@
+			${process.env.DATABASE_URL}`,
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
